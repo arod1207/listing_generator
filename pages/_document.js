14a2256c9f4690335d2351802f89d6b9d426249c
Armando Rodriguez <arod1207@me.com>
2023-01-27 15:34:03 -0600

    add google analytics and favicon

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,29 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { useState } from "react";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-ZE28HEF90Q"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-ZE28HEF90Q', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
